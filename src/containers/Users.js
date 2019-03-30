a711227faf6e50ecbe0a7e797e552bd290db87b7
Fotis Tsakiris <footios76@gmail.com>
2019-03-30 15:57:05 +0200

    400. basic React app created

---
+++ src/containers/Users.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Users extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <div>
+                <h1>The Users</h1>
+                <p>Awesome Users on board of this course!</p>
+            </div>
+          );
+    }
+}
+ 
+export default Users;
